2554de52669197c7d538cda93051f25fa4b6fae3
Denis Tumakov <dmarshaltu@gmail.com>
2022-05-30 19:47:46 +0000

    a

--- README.md
+++
@@ -1,2 +0,0 @@
-# velero_wui_py
-[![wakatime](https://wakatime.com/badge/github/DmarshalTU/velero_wui_py.svg)](https://wakatime.com/badge/github/DmarshalTU/velero_wui_py)
---
+++ src/server.py
@@ -0,0 +1,17 @@
+# import bjoern
+# from velero import app
+
+# host = '0.0.0.0'
+# port = 8000
+# bjoern.listen(app, host, port)
+# bjoern.run()
+
+from velero import app
+
+if __name__ == '__main__':
+    import bjoern
+    host = "0.0.0.0"
+    port = 5000
+
+    bjoern.listen(app, host, port)
+    bjoern.run(statsd=...)
